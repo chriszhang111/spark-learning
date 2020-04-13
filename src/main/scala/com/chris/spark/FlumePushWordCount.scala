0e69d8dc98aa90461f225e359b57b4dc42037211
chris <869415122@qq.com>
2020-04-13 10:28:00 -0400

    flume added

--- src/main/scala/com/chris/spark/FlumePushWordCount.scala
+++ src/main/scala/com/chris/spark/FlumePushWordCount.scala
@@ -8,10 +8,21 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 object FlumePushWordCount {
 
   def main(args: Array[String]): Unit = {
+
+    if(args.length != 2){
+      System.err.println("Usage: FlumePushWordCount <hostname> <port>")
+    }
+
+    val Array(hostname, port) = args
+
     val sparkConf = new SparkConf().setMaster("local[2]").setAppName("FlumePush")
     var ssc = new StreamingContext(sparkConf, Seconds(5))
 
-    FlumeUtils.createStream(ssc, "localhost", 41414)
+    val flumeStream = FlumeUtils.createStream(ssc, hostname, port.toInt)
+    flumeStream.map(x => new String(x.event.getBody.array()).trim)
+      .flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_).print()
+
+
 
     ssc.start()
     ssc.awaitTermination()
